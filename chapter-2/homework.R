06bcd2da5f6f5f21e71faf21f25b477e5adcd963
Jorge Cimentada <cimentadaj@gmail.com>
2019-07-02 17:50:09 +0200

    Updates answers from ch2

--- chapter-2/homework.R
+++ chapter-2/homework.R
@@ -1,133 +1,202 @@
+library(ggplot2)
 # homework
+
+## 2E1
+## 2
+## Option 2 is the most straight forward but (4) if also the answer
+## as it is just adjusting for the probability that it is monday
+
+
+## 2E2
+## 3
+
+## 2E3
+## 1
+## Also (5), see answer to 2E1
+
+## 2E4
+## Given our limited knowledge and the results of the experiment, we believe that the true proportion of water in the globe is 0.7.
+## Taken from http://rstudio-pubs-static.s3.amazonaws.com/326789_e280ba45916a400f9556212409406da2.html
+
 
 ## 2M1
 
-p_grid <- seq(from = 0, to = 1, length.out = 100)
-prior <- rep(x = 1, length = length(p_grid))
+compute_posterior <- function(w, n, N = 20) {
+  p_grid <- seq(0, 1, length.out = N)
+  prior <- rep(1, times = N)
 
-compute_posterior <- function(w, n, prior, p = p_grid) {
-  likelihood <- dbinom(x = w, size = n, prob = p)
-  unstandardized.posterior <- likelihood * prior
-  return( unstandardized.posterior / sum(unstandardized.posterior) )
+  likelihood <- dbinom(x = w, size = n, prob = p_grid)
+  unstd.posterior <- likelihood * prior
+  std.posterior <- unstd.posterior / sum(unstd.posterior)
+  comb_result <- list(p_grid = p_grid,
+                      posterior = std.posterior)
+  comb_result
 }
 
-plot_posterior <- function(x, y) {
-  plot(x = x, y = y, type="b", xlab = "Probability of Water", ylab = "Posterior Probability")
-  title <- paste( length(x), "Points")
-  mtext(title)
+plot_posterior <- function(p_grid, posterior) {
+  qplot(x = p_grid, y =  posterior, geom = "point",
+        xlab = "Probability there is water",
+        ylab = "Posterior probability") +
+    geom_line()
 }
 
-# (1)
-w <- 3
-n <- 3
-posterior <- compute_posterior(w = w, n = n, prior = prior)
-plot_posterior(x = p_grid, y = posterior)
 
-# (2)
-w <- 3
-n <- 4
-posterior <- compute_posterior(w = w, n = n, prior = prior)
-plot_posterior(x = p_grid, y = posterior)
+## 1)
+posterior <- compute_posterior(3, 3)
+plot_posterior(posterior$p_grid, posterior$posterior)
 
-# (3)
-w <- 5
-n <- 7
-posterior <- compute_posterior(w = w, n = n, prior = prior)
-plot_posterior(x = p_grid, y = posterior)
+## 2)
+posterior <- compute_posterior(3, 4)
+plot_posterior(posterior$p_grid, posterior$posterior)
+
+## 3)
+posterior <- compute_posterior(5, 7)
+plot_posterior(posterior$p_grid, posterior$posterior)
+
 
 ## 2M2
-prior <- ifelse(test = p_grid < .5, yes = 0, no = 1)
 
-# (1)
-w <- 3
-n <- 3
-posterior <- compute_posterior(w = w, n = n, prior = prior)
-plot_posterior(x = p_grid, y = posterior)
+compute_posterior <- function(w, n, N = 20) {
+  p_grid <- seq(0, 1, length.out = N)
+  prior <- ifelse(p_grid < 0.5, 0, .6)
+  likelihood <- dbinom(x = w, size = n, prob = p_grid)
+  unstd.posterior <- likelihood * prior
+  std.posterior <- unstd.posterior / sum(unstd.posterior)
+  comb_result <- list(p_grid = p_grid,
+                      posterior = std.posterior)
+  comb_result
+}
 
-# (2)
-w <- 3
-n <- 4
-posterior <- compute_posterior(w = w, n = n, prior = prior)
-plot_posterior(x = p_grid, y = posterior)
 
-# (3)
-w <- 5
-n <- 7
-posterior <- compute_posterior(w = w, n = n, prior = prior)
-plot_posterior(x = p_grid, y = posterior)
+## 1)
+posterior <- compute_posterior(3, 3)
+plot_posterior(posterior$p_grid, posterior$posterior)
 
-## 2M3
-prior <- c(.5, .5)
-likelihood <- c(.3, 1)
-unstandardized.posterior <- prior * likelihood
-posterior <- unstandardized.posterior / sum(unstandardized.posterior)
-round( posterior[1], 2) == .23
+## 2)
+posterior <- compute_posterior(3, 4)
+plot_posterior(posterior$p_grid, posterior$posterior)
 
-## 2M4
-card.1.likelihood <- 2
-card.2.likelihood <- 1
-card.3.likelihood <- 0
-likelihood <- c(card.1.likelihood, card.2.likelihood, card.3.likelihood)
-prior <- rep(x = 1, length = length(likelihood))
-unstandardized.posterior <- prior * likelihood
-posterior <- unstandardized.posterior / sum(unstandardized.posterior)
+## 3)
+posterior <- compute_posterior(5, 7)
+plot_posterior(posterior$p_grid, posterior$posterior)
 
-# the probability the other size is black is equal to the probability that we've drawn card 1
-posterior[1] == 2/3
+# 2M3
 
-## 2M5
-card.1.likelihood <- 2
-card.2.likelihood <- 1
-card.3.likelihood <- 0
-card.4.likelihood <- 2
-likelihood <- c(card.1.likelihood, card.2.likelihood, card.3.likelihood, card.4.likelihood)
-prior <- rep(x = 1, length = length(likelihood))
-unstandardized.posterior <- prior * likelihood
-posterior <- unstandardized.posterior / sum(unstandardized.posterior)
+likelihood <- c('earth' = .5, 'mars' = .5)
+prior <- c('earth' = .3, 'mars' = 1)
 
-# the probability the other size is black is equal to the probability that we've drawn card 1 or 4
-posterior[1] + posterior[4]
+unstd.posterior <- likelihood * prior
+std.posterior <- unstd.posterior / sum(unstd.posterior)
+round(std.posterior, 2)
 
-## 2M6
-card.1.likelihood <- 2
-card.2.likelihood <- 1
-card.3.likelihood <- 0
-likelihood <- c(card.1.likelihood, card.2.likelihood, card.3.likelihood)
-prior <- c(1, 2, 3)
-unstandardized.posterior <- prior * likelihood
-posterior <- unstandardized.posterior / sum(unstandardized.posterior)
 
-# the probability the other size is black is equal to the probability that we've drawn card 1
-posterior[1] == .5
+tst
 
-## 2M7
-card.1.likelihood <- 2
-card.2.likelihood <- 1
-card.3.likelihood <- 0
-likelihood <- c(card.1.likelihood, card.2.likelihood, card.3.likelihood)
-prior <- c(1, 2, 3)
-unstandardized.posterior <- prior * likelihood
-posterior <- unstandardized.posterior / sum(unstandardized.posterior)
+# 2M4
 
-# the probability the other size is black is equal to the probability that we've drawn card 1
-posterior[1] == .5
+# I counted this manually!
+# For the B/B card
+#    B     B
+#     B - B
+# The first B's mean each card's face and the other tree
+# is the opposite side. Here there are two ways to get B/B
+# Given that we do not introduce the card back to the bag,
+# we do not have two outcomes as McElreath's example with
+# the
 
-## 2M8
-card.1.2.likelihood <- 2
-card.2.1.likelihood <- 0
-card.1.3.likelihood <- 4
-card.3.1.likelihood <- 0
-card.2.3.likelihood <- 2
-card.3.2.likelihood <- 0
+# For the B/W card
+#  W     B
+#   B - W
+# Here there's only one way to get the first side as black
 
-likelihood <- c(card.1.2.likelihood, card.2.1.likelihood, card.1.3.likelihood, card.3.1.likelihood, card.2.3.likelihood, card.3.2.likelihood)
-prior <- rep(x = 1, length = length(likelihood))
-unstandardized.posterior <- prior * likelihood
-posterior <- unstandardized.posterior / sum(unstandardized.posterior)
+# The question asked what is probability of getting a black on the other side.
+# For that we only have to take proportion of B/B combinations from what we
+# counted above. 
 
-# the probability that the other side of the first card is black is equal to the probability that the first card is card 1,
-# which equals the probability that the sequence we've chosen is either (1, 2), or (1, 3)
-posterior[1] + posterior[3] == .75
+black_cards <- c('B/B' = 2, 'B/W' = 1, 'W/W' = 0)
+# No need to multiply by the prior because they are all equally likely
+# so it would be the same as multiplying by 1
+
+res <- black_cards / sum(black_cards)
+
+# This is probability that it will be B/B
+res['B/B']
+
+
+# 2M5
+# Here B/B is 4 because there are now 2 cards with B/B
+black_cards <- c('B/B' = 4, 'B/W' = 1, 'W/W' = 0)
+# No need to multiply by the prior because they are all equally likely
+# so it would be the same as multiplying by 1
+
+res <- black_cards / sum(black_cards)
+
+# This is probability that it will be B/B
+res['B/B']
+
+# 2M6
+black_cards <- c('B/B' = 2, 'B/W' = 1, 'W/W' = 0)
+prior <- c('B/B' = 1, 'B/W' = 2, 'W/W' = 3)
+
+# This is likelihood * prior
+unstd.posterior <- black_cards * prior
+
+std.posterior <- unstd.posterior / sum(unstd.posterior)
+
+# This is probability that it will be B/B
+std.posterior['B/B']
+
+# 2M7
+# I did not understood this one. I'm copying the answer from https://github.com/jffist/statistical-rethinking-solutions/blob/master/ch02_hw.R
+# but I'm adding an explanation to my logic behind it.
+
+# For the black card, there are 2 possibility of getting black in the first side.
+# We multiply it by the number of possible whites if thiis is the first card
+card.bb.likelihood <- 2*3
+
+# For the W/B card, we only have one black but there is the possibility
+# of having 2 whites from card W/W
+card.wb.likelihood <- 1*2
+
+# From card W/W there is no blacks so anything multipled by 0 will be 0
+card.ww.likelihood <- 0
+
+# No need to multiply by the prior (1's) because it's the same in this case
+posterior <- c(card.bb.likelihood, card.wb.likelihood, card.ww.likelihood)
+
+posterior <- posterior/sum(posterior)
+
+# The probability that the first card is B/B is now 0.75
+posterior[1]
+
+# You can think of as the the computations above as the # of blacks
+# adjusted by the possible number of whites
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## 2H1
 
